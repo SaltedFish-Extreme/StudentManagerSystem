751cbccc196d3058dff3f2c7e9cacd494eb13e32
咸鱼至尊 <1738099872@qq.com>
2024-12-11 17:40:40 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# 从studentManager模块导入StudentManagerSystem功能
+from studentManager import StudentManagerSystem
+
+# 定义入口代码
+if __name__ == '__main__':
+    student_manager = StudentManagerSystem()
+    student_manager.run()
